1ac85b171ac5d70539f4074907873d1c40ef78b6
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-12 22:26:13 -0400

    donE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team_info = params
+      erb :team
+    end
+
 
 end
